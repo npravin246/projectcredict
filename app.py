4863b1a27b7fa96ec91a304b35dce4f967d9e3a2
npravin246 <npravin246@gmail.com>
2026-03-07 15:55:46 +0530

    Enable dynamic port configuration for app
    Added support for dynamic port configuration in production.

--- app.py
+++ app.py
@@ -70,4 +70,11 @@ def predict():
         return render_template("index.html", prediction_text="Invalid Input ❌")
 
 if __name__ == "__main__":
+
     app.run()
+    
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
